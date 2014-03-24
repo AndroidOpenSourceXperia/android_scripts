cd84bcd5cb83783effb35fd897b191781e1be4d2
95A31 <x95a31x@gmail.com>
2014-03-24 11:44:39 +0100

    Update scripts

--- CRUC.sh
+++ CRUC.sh
@@ -1,1 +1,14 @@
-./CleanCM.sh && ./ResetCM.sh && ./UpdateCM.sh && ./CompileCM.sh
+if [[ $# = 1 ]]; then
+  case $1 in
+  -u|-p|-s|-g)
+  ;;
+  *)
+    echo "ERROR: Unknow option"
+    exit 1;
+  ;;
+  esac
+else 
+   echo "ERROR: Number of options not correct. Usage: ./Compile.sh -u | -p | -s | -g"
+fi
+
+./CleanCM.sh && ./ResetCM.sh && ./UpdateCM.sh && ./CompileCM.sh $1
