13356667fcf515534ffbfdebd0384ce62615f68c
95A31 <x95a31x@gmail.com>
2014-03-27 10:23:34 +0100

    Checrry pick experiments x8

--- Compile.sh
+++ Compile.sh
@@ -13,10 +13,34 @@ else
 fi
 
 #Set patches directory
+rootDir=$PWD
 patchDir="hardware/ste/patches"
 
+# Cherry picks
+echo "Cherry picking..."
+old_IFS=$IFS      
+IFS=$'\n'          
+for line in $(cat $patchDir/cherry-picks.list | grep -v "#")          
+do          
+   dir=$(echo $line | cut -f1 -b "|");
+   gitCmd=$(echo $line | cut -f2 -b "|");
+   
+   cd $dir;
+   git $gitCmd;
+   exitValue=$?
+   cd $rootdir;
+   
+    if [[ ! $? ]]; then
+      echo "Error with cherry-pick $gitCmd in $dir";
+      exit 1;
+    fi
+   
+done          
+IFS=$old_IFS
+
+
 #Patch
-echo Patching...
+echo "Patching..."
 for d in $(ls $patchDir | grep -v FM);
 do
   for f in $(ls $patchDir"/"$d | grep .patch);
@@ -32,33 +56,6 @@ done
 
 # We use CyanogenMod su binary, remove AOSP one in order to avoid conflicts
 rm system/extras/su/Android.mk &> /dev/null
-
-# Cherry picks
-echo "Cherry picking..."
-rootDir=$PWD
-
-# Support for Make 4.0
-cherryPick=7a77261988354b13996e5ba84b87351b571a5a52
-if [[ ! $(cd build && git cherry-pick -m 1 $cherryPick && cd $rootDir) ]]; then
-  echo "Error with cherry-pick $cherryPick"; exit 1;
-fi
-
-# Java 7 compilation
-cherryPick=ad6909913145ba3daeb037f0170b95f855f7d08f
-if [[ ! $(cd build && git cherry-pick $cherryPick && cd $rootDir) ]]; then
-  echo "Error with cherry-pick $cherryPick"; exit 1;
-  exit 1;
-fi
-cherryPick=a9a297845620cfe56bbe3b9f8de1331b7ec54574
-if [[ ! $(cd libcore && git cherry-pick $cherryPick && cd $rootDir) ]]; then
-  echo "Error with cherry-pick $cherryPick"; exit 1;
-fi
-
-# Fix undefined symbol '_ZN7android10MemoryBaseC1ERKNS_2spINS_11IMemoryHeapEEElj'
-cherryPick=ddd286301b466b5cd10cfcdc0a5ee5729ef2e385
-if [[ ! $(cd frameworks/native && git cherry-pick $cherryPick && cd $rootDir) ]]; then
-  echo "Error with cherry-pick $cherryPick"; exit 1;
-fi
 
 #Recompile
 ./Recompile.sh $1
