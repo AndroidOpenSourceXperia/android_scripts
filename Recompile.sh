5eac0f9323cdc9a1d98e7ceb3c148ee581a5e7c4
95A31 <x95a31x@gmail.com>
2014-03-28 00:08:51 +0100

    Switch to java 6 x3

--- Recompile.sh
+++ Recompile.sh
@@ -6,7 +6,7 @@ if [[ $# = 1 ]]; then
     # Use local Java Development Kit 6
     if (( $(java -version 2>&1 | grep version | cut -f2 -d".") > 6 )); then
        echo "Using local JDK 6..."
-       export JAVA_HOME=$(realpath ../jdk1.6.0_45/bin/);
+       export JAVA_HOME=$(realpath ../jdk1.6.0_45);
        export ANDROID_JAVA_HOME=$JAVA_HOME;
     fi
     
