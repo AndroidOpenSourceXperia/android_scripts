9caa68e7d7f7c4830d2696bf68d4e7844068a3f0
95A31 <x95a31x@gmail.com>
2014-03-28 11:04:08 +0100

    Swith to eng also here

--- CompileRamdisk.sh
+++ CompileRamdisk.sh
@@ -6,16 +6,16 @@ if [[ $# = 1 ]]; then
     
     case $1 in
     -u)
-      lunch full_kumquat-userdebug && make bootimage;
+      lunch full_kumquat-eng && make bootimage;
     ;;
     -p)
-      lunch full_nypon-userdebug && make bootimage;
+      lunch full_nypon-eng && make bootimage;
     ;;
     -s)
-      lunch full_pepper-userdebug && make bootimage;
+      lunch full_pepper-eng && make bootimage;
     ;;
     -g)
-      lunch full_lotus-userdebug && make bootimage;
+      lunch full_lotus-eng && make bootimage;
     ;;
     *)
       echo "ERROR: Unknow option";
