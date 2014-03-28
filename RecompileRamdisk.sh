cda582221747a50a4eb4b82a2dfafbae6203c752
95A31 <x95a31x@gmail.com>
2014-03-28 09:53:59 +0100

    Update RecompileRamdisk to AOSP

--- RecompileRamdisk.sh
+++ RecompileRamdisk.sh
@@ -1,2 +1,32 @@
 #Compile
-. build/envsetup.sh && breakfast kumquat && make bootimage;
+
+if [[ $# = 1 ]]; then
+  . build/envsetup.sh
+  if [[ $? = 0 ]]; then
+    
+    case $1 in
+    -u)
+      lunch full_kumquat-userdebug && make bootimage;
+    ;;
+    -p)
+      lunch full_nypon-userdebug && make bootimage;
+    ;;
+    -s)
+      lunch full_pepper-userdebug && make bootimage;
+    ;;
+    -g)
+      lunch full_lotus-userdebug && make bootimage;
+    ;;
+    *)
+      echo "ERROR: Unknow option";
+      exit 1;
+    ;;
+    esac
+  else 
+    echo "ERROR: . build/envsetup.sh falied"
+    exit 1;
+  fi
+else
+  echo "ERROR: Number of options not correct. Usage: ./RecompileRamdisk.sh -u | -p | -s | -g"
+  exit 1;
+fi
