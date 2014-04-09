5b00767a117a1897b295210fc931f3707c8e818f
95A31 <x95a31x@gmail.com>
2014-04-09 09:47:23 +0200

    Fix Patch + moved some stuff from Patch to Update

--- Patch.sh
+++ Patch.sh
@@ -35,17 +35,10 @@ do
     RETVAL=$?
     if [ $RETVAL -ne 0 ]; then
 	  echo "Error $RETVAL with patch $dir$f"
-	  exit $RETVAL
+	  exit -1
     fi
   done
 done
 
 # We use CyanogenMod su binary, remove AOSP one in order to avoid conflicts
 rm system/extras/su/Android.mk &> /dev/null
-
-# Dhcpd rules needed by Wi-Fi Direct
-curl https://raw.githubusercontent.com/CyanogenMod/android_hardware_qcom_wlan/cm-11.0/qcwcn/config/android_dhcpcd.conf > device/sony/montblanc-common/config/dhcpcd.conf
-
-# Missing TI's files in AOSP
-curl https://raw.githubusercontent.com/CyanogenMod/android_system_netd/cm-11.0/SoftapControllerTI.cpp > system/netd/SoftapControllerTI.cpp
-curl https://raw.githubusercontent.com/CyanogenMod/android_system_netd/cm-11.0/SoftapControllerTI.h > system/netd/SoftapControllerTI.h
--- Update.sh
+++ Update.sh
@@ -1,2 +1,8 @@
 git pull && ./repo sync -cq -j8
 
+# Dhcpd rules needed by Wi-Fi Direct
+curl https://raw.githubusercontent.com/CyanogenMod/android_hardware_qcom_wlan/cm-11.0/qcwcn/config/android_dhcpcd.conf > device/sony/montblanc-common/config/dhcpcd.conf
+
+# Missing TI's files in AOSP
+curl https://raw.githubusercontent.com/CyanogenMod/android_system_netd/cm-11.0/SoftapControllerTI.cpp > system/netd/SoftapControllerTI.cpp
+curl https://raw.githubusercontent.com/CyanogenMod/android_system_netd/cm-11.0/SoftapControllerTI.h > system/netd/SoftapControllerTI.h
