10d18e43c89dc825ac3e465369a3846a30eeb285
95A31 <x95a31xreg@gmail.com>
2014-08-26 21:05:29 +0200

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -3,9 +3,5 @@ git pull && ./repo sync -cq -j8
 # Dhcpd rules needed by Wi-Fi Direct
 curl https://raw.githubusercontent.com/CyanogenMod/android_hardware_qcom_wlan/cm-11.0/qcwcn/config/android_dhcpcd.conf > device/sony/montblanc-common/config/dhcpcd.conf
 
-# Missing TI's files in AOSP
-curl https://raw.githubusercontent.com/CyanogenMod/android_system_netd/cm-11.0/SoftapControllerTI.cpp > system/netd/SoftapControllerTI.cpp
-curl https://raw.githubusercontent.com/CyanogenMod/android_system_netd/cm-11.0/SoftapControllerTI.h > system/netd/SoftapControllerTI.h
-
 # APNs list
 curl https://raw.githubusercontent.com/CyanogenMod/android_vendor_cm/cm-11.0/prebuilt/common/etc/apns-conf.xml > device/generic/goldfish/data/etc/apns-conf.xml
